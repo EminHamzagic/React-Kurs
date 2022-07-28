75a849839eaaba60e8d0cc9e9af3c2800c7540be
Emin Hamzagic <hamzagicemin4@gmail.com>
2022-07-28 18:21:13 +0200

    vezbanje

--- src/components/card.js
+++ src/components/card.js
@@ -1,23 +1,20 @@
-import "../card.css";
+import "../css/card.css";
 
 export default function Card() {
   return (
-    <div className="container">
-      <div className="background"></div>
-      <div className="card">
-        <img
-          alt=""
-          src="https://www.pngitem.com/pimgs/m/30-300674_computer-icons-download-clip-art-twitter-icon-black.png"
-        />
-        <h1>TWITTER</h1>
-        <p>
-          Unpleasant astonished an diminution up partiality. Noisy an their of
-          meant. Death means up civil do an offer wound of. Called square an in
-          afraid direct. Resolution diminution conviction so mr at unpleasing
-          simplicity no.
-        </p>
-        <button>READ MORE</button>
-      </div>
+    <div className="card">
+      <img
+        alt=""
+        src="https://www.pngitem.com/pimgs/m/30-300674_computer-icons-download-clip-art-twitter-icon-black.png"
+      />
+      <h1>TWITTER</h1>
+      <p>
+        Unpleasant astonished an diminution up partiality. Noisy an their of
+        meant. Death means up civil do an offer wound of. Called square an in
+        afraid direct. Resolution diminution conviction so mr at unpleasing
+        simplicity no.
+      </p>
+      <button>READ MORE</button>
     </div>
   );
 }
