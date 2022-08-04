6b0e1495885cd5e7f5ec7c552ca0bdaffcc10a94
Emin Hamzagic <hamzagicemin4@gmail.com>
2022-08-04 18:25:03 +0200

    calculator react

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,152 +13,161 @@ export default function Calculator() {
   return (
     <div className="container">
       <div className="screen">{expression}</div>
-      <div className="btnBox">
-        <button
-          id="7"
-          className="btn"
-          onClick={(e) => {
-            changeScreen(e.target.id);
-          }}
-        >
-          7
-        </button>
-        <button
-          id="8"
-          className="btn"
-          onClick={(e) => {
-            changeScreen(e.target.id);
-          }}
-        >
-          8
-        </button>
-        <button
-          id="9"
-          className="btn"
-          onClick={(e) => {
-            changeScreen(e.target.id);
-          }}
-        >
-          9
-        </button>
-        <button
-          id="+"
-          className="btn"
-          onClick={(e) => {
-            changeScreen(e.target.id);
-          }}
-        >
-          +
-        </button>
-        <button
-          id="4"
-          className="btn"
-          onClick={(e) => {
-            changeScreen(e.target.id);
-          }}
-        >
-          4
-        </button>
-        <button
-          id="5"
-          className="btn"
-          onClick={(e) => {
-            changeScreen(e.target.id);
-          }}
-        >
-          5
-        </button>
-        <button
-          id="6"
-          className="btn"
-          onClick={(e) => {
-            changeScreen(e.target.id);
-          }}
-        >
-          6
-        </button>
-        <button
-          id="-"
-          className="btn"
-          onClick={(e) => {
-            changeScreen(e.target.id);
-          }}
-        >
-          -
-        </button>
-        <button
-          id="1"
-          className="btn"
-          onClick={(e) => {
-            changeScreen(e.target.id);
-          }}
-        >
-          1
-        </button>
-        <button
-          id="2"
-          className="btn"
-          onClick={(e) => {
-            changeScreen(e.target.id);
-          }}
-        >
-          2
-        </button>
-        <button
-          id="3"
-          className="btn"
-          onClick={(e) => {
-            changeScreen(e.target.id);
-          }}
-        >
-          3
-        </button>
-        <button
-          id="*"
-          className="btn"
-          onClick={(e) => {
-            changeScreen(e.target.id);
-          }}
-        >
-          *
-        </button>
-        <button
-          id="."
-          className="btn"
-          onClick={(e) => {
-            changeScreen(e.target.id);
-          }}
-        >
-          .
-        </button>
-        <button
-          id="0"
-          className="btn"
-          onClick={(e) => {
-            changeScreen(e.target.id);
-          }}
-        >
-          0
-        </button>
-        <button
-          id="="
-          className="btn"
-          onClick={() => {
-            setExpression(eval(expression));
-          }}
-        >
-          =
-        </button>
-        <button
-          id="/"
-          className="btn"
-          onClick={(e) => {
-            changeScreen(e.target.id);
-          }}
-        >
-          /
-        </button>
-      </div>
+      <button
+        id="del"
+        className="btn"
+        onClick={(e) => {
+          setExpression((prev) => {
+            return prev.slice(0, prev.length - 1);
+          });
+        }}
+      >
+        del
+      </button>
+      <button
+        id="7"
+        className="btn"
+        onClick={(e) => {
+          changeScreen(e.target.id);
+        }}
+      >
+        7
+      </button>
+      <button
+        id="8"
+        className="btn"
+        onClick={(e) => {
+          changeScreen(e.target.id);
+        }}
+      >
+        8
+      </button>
+      <button
+        id="9"
+        className="btn"
+        onClick={(e) => {
+          changeScreen(e.target.id);
+        }}
+      >
+        9
+      </button>
+      <button
+        id="+"
+        className="btn"
+        onClick={(e) => {
+          changeScreen(e.target.id);
+        }}
+      >
+        +
+      </button>
+      <button
+        id="4"
+        className="btn"
+        onClick={(e) => {
+          changeScreen(e.target.id);
+        }}
+      >
+        4
+      </button>
+      <button
+        id="5"
+        className="btn"
+        onClick={(e) => {
+          changeScreen(e.target.id);
+        }}
+      >
+        5
+      </button>
+      <button
+        id="6"
+        className="btn"
+        onClick={(e) => {
+          changeScreen(e.target.id);
+        }}
+      >
+        6
+      </button>
+      <button
+        id="-"
+        className="btn"
+        onClick={(e) => {
+          changeScreen(e.target.id);
+        }}
+      >
+        -
+      </button>
+      <button
+        id="1"
+        className="btn"
+        onClick={(e) => {
+          changeScreen(e.target.id);
+        }}
+      >
+        1
+      </button>
+      <button
+        id="2"
+        className="btn"
+        onClick={(e) => {
+          changeScreen(e.target.id);
+        }}
+      >
+        2
+      </button>
+      <button
+        id="3"
+        className="btn"
+        onClick={(e) => {
+          changeScreen(e.target.id);
+        }}
+      >
+        3
+      </button>
+      <button
+        id="*"
+        className="btn"
+        onClick={(e) => {
+          changeScreen(e.target.id);
+        }}
+      >
+        *
+      </button>
+      <button
+        id="."
+        className="btn"
+        onClick={(e) => {
+          changeScreen(e.target.id);
+        }}
+      >
+        .
+      </button>
+      <button
+        id="0"
+        className="btn"
+        onClick={(e) => {
+          changeScreen(e.target.id);
+        }}
+      >
+        0
+      </button>
+      <button
+        id="="
+        className="btn"
+        onClick={() => {
+          setExpression(eval(expression));
+        }}
+      >
+        =
+      </button>
+      <button
+        id="/"
+        className="btn"
+        onClick={(e) => {
+          changeScreen(e.target.id);
+        }}
+      >
+        /
+      </button>
     </div>
   );
 }
