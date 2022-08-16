ce74404865773a604b70c99363cf9f554edfb59a
Emin Hamzagic <hamzagicemin4@gmail.com>
2022-08-16 18:14:47 +0200

    finished radnom word gen

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -2,35 +2,32 @@ import { useState, useEffect } from "react";
 import "../css/hangman.css";
 
 export default function Hangman() {
-  const [tires, setTries] = useState("6");
+  const [tries, setTries] = useState(6);
   const [word, setWord] = useState([]);
 
   const checkChar = (e) => {
     if (e.keyCode == 13) {
-      console.log("nice");
+      setTries(tries - 1);
     }
   };
 
-  async function getWord() {
-    var respone = await fetch(`https://random-words-api.vercel.app/word`);
-    var data = await respone.json();
-    console.log(data.word);
-    return data;
-  }
-
   useEffect(() => {
-    getWord().then((data) => console.log(data.word));
+    fetch(`https://random-words-api.vercel.app/word`)
+      .then((response) => response.json())
+      .then((data) => setWord(data[0].word.split("")));
   }, []);
 
   return (
     <div className="container">
       <div className="infoAndInput">
         <input onKeyDown={checkChar} maxLength={1} type="text"></input>
-        <h1>Number of tries left: {tires}</h1>
+        <h1>Number of tries left: {tries}</h1>
       </div>
-      {/* {word.map((word) => (
-        <p>{word}</p>
-      ))} */}
+      <div className="wordCont">
+        {word.map((char, index) => (
+          <p key={char + index.toString()}>_&nbsp;</p>
+        ))}
+      </div>
     </div>
   );
 }
