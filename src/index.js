9ea214a866154f3cab194bb9af1225526a4206aa
Emin Hamzagic <hamzagicemin4@gmail.com>
2022-09-03 18:10:16 +0200

    route vezbanje

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 // import Hangman from "./components/Hangman.js";
 import "./index.css";
 import ToDo from "./ToDo/ToDo.js";
+import { Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <BrowserRouter>
+    <Header />
+    <div className="container">
+      <Route />
+    </div>
+  </BrowserRouter>
   // <React.StrictMode>
-  <ToDo />
   // </React.StrictMode>
 );
